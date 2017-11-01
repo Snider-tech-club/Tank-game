7ba0d0aa4709e797402c5bc0bd8b296264040b5d
jordandavis19 <31890827+jordandavis19@users.noreply.github.com>
2017-11-01 19:54:44 -0400

    Update normal_mode.lua

--- game.love/graphics/normal_mode.lua
+++ game.love/graphics/normal_mode.lua
@@ -1,12 +1,12 @@
 player = { x = 200, y = 200, speed = 150, img = nil }
 canShoot = true
-canShootTimerMax = 0.2
+canShootTimerMax = 0.8
 canShootTimer = canShootTimerMax
 bulletImg = nil
 
 bullets = {}
 function love.draw(dt)
-
+    
 end
 
 function love.load(arg)
@@ -19,16 +19,16 @@ if love.keyboard.isDown('escape') then
 		love.event.push('quit')
 	end
 
-	if love.keyboard.isDown('left','a') then
-		if player.x > 0 then
-		player.x = player.x - (player.speed*dt)
+	if love.keyboard.isDown('left','a') then 
+		if player.x > 0 then 
+		player.rotation = math.deg(player.x - player.speed*dt)
 	end
 	elseif love.keyboard.isDown('right','d') then
 		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
-		player.x = player.x + (player.speed*dt)
-	end
-    elseif love.keyboard.isDown('down','w') then
-		if player.y > 0 then
+		player.rotation = math.deg(player.x + player.speed*dt)
+	end	
+    elseif love.keyboard.isDown('down','w') then 
+		if player.y > 0 then 
 		player.y = player.y - (player.speed*dt)
 	end
     elseif love.keyboard.isDown('up','s') then
@@ -36,19 +36,19 @@ if love.keyboard.isDown('escape') then
 		player.y = player.y + (player.speed*dt)
 	end
     elseif love.keyboard.isDown('space') and canShoot then
-        newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
+        newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }	
 	table.insert(bullets, newBullet)
 	canShoot = true
 	CanShootTimer = canShootTimerMax
 	end
-	canShootTimer = canShootTimer - (1 * dt)
+	canShootTimer = canShootTimer - (1 * dt) 
 if canShootTimer < 0 then
 	canShoot = true
 end
 for i, bullet in ipairs(bullets) do
 	bullet.y = bullet.y - (250 * dt)
 
-	if bullet.y < 0 then
+	if bullet.y < 0 then 
 		table.remove(bullets, i)
 	end
 end
@@ -57,8 +57,8 @@ end
 
 
 function love.draw(dt)
-	love.graphics.draw(player.img, player.x, player.y)
-
+	love.graphics.draw(player.img, player.x, player.y, player.rotation, 1, 1, player.img:getWidth()/2, player.img:getHeight()/2)
+	
 for i, bullet in ipairs(bullets) do
 	love.graphics.draw(bullet.img, bullet.x, bullet.y)
 end
